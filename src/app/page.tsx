62f44e9e52b3a2b31b44d15b77dcb6f3c04e8672
Roy Barneveld <roy.barneveld1@gmail.com>
2024-07-12 22:03:59 +0200

    add walletconnect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import Image from "next/image";
+import ConnectButton from "@/components/ConnectButton";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-center font-mono text-sm lg:flex">
-        LazyLP
-      </div>
-    </main>
-  );
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div className="z-10 w-full max-w-5xl items-center justify-center font-mono text-sm f flex flex-col">
+                Lazy LP
+                <ConnectButton/>
+            </div>
+        </main>
+    );
 }
---
+++ src/config/index.tsx
@@ -0,0 +1,32 @@
+import {defaultWagmiConfig} from '@web3modal/wagmi/react/config'
+
+import {cookieStorage, createStorage} from 'wagmi'
+import {mainnet, sepolia} from 'wagmi/chains'
+
+export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
+
+if (!projectId) throw new Error('Project ID is not defined')
+
+const metadata = {
+    name: 'LazyLP',
+    description: 'AI powered liquidity provision',
+    url: 'lazy-lp.vercel.app', // origin must match your domain & subdomain
+    icons: ['https://avatars.githubusercontent.com/u/37784886']
+}
+
+const chains = [mainnet, sepolia] as const
+export const config = defaultWagmiConfig({
+    chains,
+    projectId,
+    metadata,
+    ssr: true,
+    storage: createStorage({
+        storage: cookieStorage
+    }),
+    auth: {
+        email: true, // default to true
+        socials: ['google', 'x', 'github', 'discord', 'apple'],
+        showWallets: true, // default to true
+        walletFeatures: true // default to true
+    }
+})
