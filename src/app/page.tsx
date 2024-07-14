f76e88e589bb4608eef793aa74236c138ec668db
jyanon <804173948@qq.com>
2024-07-14 02:58:07 +0200

    UseSwap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,7 @@ import {useRouter} from "next/navigation";
 import {useAddLiquidity} from "../contract/useAddLiquidity";
 import {Contracts} from "../contract/abi";
 import {usePrice} from "../contract/usePrice";
+import {useSwap} from "../contract/useSwap";
 
 export default function Main() {
 
@@ -53,8 +54,16 @@ export default function Main() {
         router.push('/dashboard');
     }
 
+    const {swap} = useSwap(
+      Contracts["WETH"], Contracts["EETH"], BigInt(1 * 10 ** 18)
+    );
+
     return (
         <main className="container flex justify-center gap-8 flex-col my-20">
+
+            <button className="bg-[#333] w-[128px] m-auto" onClick={() => {
+                swap();
+            }}>Swap Once ()</button>
 
             <h1 className="text-4xl font-normal text-center">Deposit anytime, <br/> anywhere</h1>
             <Card className="bg-background rounded-xl overflow-hidden p-2 max-w-lg self-center">
---
+++ src/contract/useSwap.ts
@@ -0,0 +1,88 @@
+import {computePoolAddress, FeeAmount, Pool, Position, Route} from "@uniswap/v3-sdk";
+import {Token} from "@uniswap/sdk-core";
+import {ABIs, ChainId, Contracts} from "./abi";
+import {DefaultDecimals, DefaultFee, DefaultTickSpacing, price2Tick} from "./useAddLiquidity";
+import {useAccount, useReadContracts, useSendTransaction, useWaitForTransactionReceipt, useWriteContract} from "wagmi";
+import {BigNumber} from "@ethersproject/bignumber";
+import {formatUnits, parseEther} from "viem";
+import {useEffect, useState} from "react";
+
+export function useSwap(
+  tokenInAddress: string,
+  tokenOutAddress: string,
+  amountIn: bigint
+  ) {
+  const [step, setStep] = useState<number>(0)
+  const { writeContractAsync } = useWriteContract();
+
+  const [approvalHash1, setApprovalHash1] = useState<string>("");
+  const [depositHash, setDepositHash] = useState<string>("");
+
+  const {address} = useAccount()
+
+  const { data: approveResult1 } = useWaitForTransactionReceipt({
+    hash: approvalHash1 as `0x${string}`,
+    confirmations: 2,
+  });
+  const { data: depositResult, error: depositError } =
+    useWaitForTransactionReceipt({
+      hash: depositHash as `0x${string}`,
+      confirmations: 2,
+    });
+
+  const swap = async () => {
+    console.log("Adding liquidity...")
+    await _approve1()
+  }
+
+  const _approve1 = async () => {
+    setStep(1)
+    const hash1 = await writeContractAsync({
+      abi: ABIs["MockERC20"],
+      address: tokenInAddress,
+      functionName: "approve",
+      args: [
+        Contracts["SwapRouter02"],
+        amountIn * 12n / 10n,
+      ],
+    });
+    setApprovalHash1(hash1);
+  }
+  const _swap = async () => {
+    setStep(2)
+    const hash = await writeContractAsync({
+      abi: ABIs["SwapRouter02"],
+      address: Contracts["SwapRouter02"],
+      functionName: "exactInputSingle",
+      args: [
+        {
+          tokenIn: tokenInAddress,
+          tokenOut: tokenOutAddress,
+          fee: DefaultFee,
+          recipient: address,
+          deadline: Math.floor(Date.now() / 1000) + 60 * 20,
+          amountIn: amountIn.toString(),
+          amountOutMinimum: 0,
+          sqrtPriceLimitX96: 0,
+        },
+      ],
+    });
+    setDepositHash(hash);
+  }
+
+  useEffect(() => {
+    if (approveResult1) {
+      _swap();
+    }
+  }, [approveResult1]);
+
+  useEffect(() => {
+    if (depositResult) {
+      setStep(3);
+    }
+  }, [depositResult]);
+
+  return {
+    swap,
+  }
+}
